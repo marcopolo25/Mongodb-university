4965c7fe6167a684b94298c8e627b05253bd9305
unknown <MarcoPolo@MarcoPolo-PC.acpent.org>
2015-01-14 17:24:58 -0500

    MongoDB University HomeWork 2 - begin

---
+++ wk2/ex1/app.js
@@ -0,0 +1,33 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+    if(err) throw err;
+
+    var query = { 'grade' : {'$gt' : 69, '$lt' : 80 }};
+
+    db.collection('grades').find(query).toArray(function(err, docs) {
+        if(err) throw err;
+
+        console.dir(docs);
+
+        db.close();
+    });
+});
+
+
+/*MongoClient.connect('mongodb://localhost:27017/course', function(err, db) 
+{
+     if(err) throw err;
+
+     var query = { 'grade' : 100};
+
+     function callback(err, doc) {
+          if(err) throw err;
+
+          console.dir(doc);
+
+          db.close();
+     } 
+     
+     db.collection('grades').findOne(query, callback);
+});*/
